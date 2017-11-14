644e20e92f6187028fa20ad0b3d38de6a7303400
UlyJsi <plaksiy_ulyanka@ukr.net>
2017-11-14 19:42:11 +0100

    finished prod setting prod routes

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+// keys.js - logic to decide which credentials to return
+
+if (process.env.NODE_ENV === "production") {
+    // return keys of production
+    module.exports = require("./prod");
+} else {
+    // return keys of development
+    module.exports = require("./dev");
+}
---
+++ config/prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+  // console.dev.google
+  googleClientID: process.env.GOOGLE_CLIENT_ID, // can be shown
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET, // NOT can be shown
+  mongoURI: process_MONGO_URI, // "mlab.com" (mongoDB)
+  cookieKey: process.env.COOKIE_KEY
+};
