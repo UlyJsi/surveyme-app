cf4ec26e59aefda97c91f0f298330885e3501547
UlyJsi <plaksiy_ulyanka@ukr.net>
2017-11-14 20:54:31 +0100

    facebook dev

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,8 @@
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID, // can be shown
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET, // NOT can be shown
+  facebookClientID: process.env.FACEBOOK_CLIENT_ID,
+  facebookClientSecret: process.env.FACEBOOK_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI, // "mlab.com" (mongoDB)
   cookieKey: process.env.COOKIE_KEY
 };
--- services/passport.js
+++ services/passport.js
@@ -39,24 +39,25 @@ passport.use(
     }
   )
 );
-// passport.use(
-//   new FacebookStrategy(
-//     {
-//       clientID: keys.facebookClientID,
-//       clientSecret: keys.facebookClientSecret,
-//       callbackURL: "/auth/facebook/callback"
-//     },
-//     (accessToken, refreshToken, profile, done) => {
-//       User.findOne({ facebookId: profile.id }).then(existingUser => {
-//         if (existingUser) {
-//           // have already record
-//           done(null, existingUser);
-//         } else {
-//           new User({ facebookId: profile.id })
-//             .save()
-//             .then(user => done(null, user));
-//         }
-//       });
-//     }
-//   )
-// );
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: keys.facebookClientID,
+      clientSecret: keys.facebookClientSecret,
+      callbackURL: "/auth/facebook/callback",
+      proxy: true
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ facebookId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          // have already record
+          done(null, existingUser);
+        } else {
+          new User({ facebookId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
+    }
+  )
+);
