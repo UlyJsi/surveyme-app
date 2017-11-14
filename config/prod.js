19cd22eb48b02d773abc5855859a2f4a42bfc923
UlyJsi <plaksiy_ulyanka@ukr.net>
2017-11-14 19:45:30 +0100

    fix typo

--- config/prod.js
+++ config/prod.js
@@ -2,6 +2,6 @@ module.exports = {
   // console.dev.google
   googleClientID: process.env.GOOGLE_CLIENT_ID, // can be shown
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET, // NOT can be shown
-  mongoURI: process_MONGO_URI, // "mlab.com" (mongoDB)
+  mongoURI: process.env.MONGO_URI, // "mlab.com" (mongoDB)
   cookieKey: process.env.COOKIE_KEY
 };
