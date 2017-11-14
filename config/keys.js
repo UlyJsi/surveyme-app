5e112424250d8a627b8447d23104b29e64a869da
UlyJsi <plaksiy_ulyanka@ukr.net>
2017-11-14 19:56:59 +0100

    fix typo_II

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 // keys.js - logic to decide which credentials to return
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV == "production") {
     // return keys of production
     module.exports = require("./prod");
 } else {
--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,4 @@
 module.exports = {
-  // console.dev.google
   googleClientID: process.env.GOOGLE_CLIENT_ID, // can be shown
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET, // NOT can be shown
   mongoURI: process.env.MONGO_URI, // "mlab.com" (mongoDB)
